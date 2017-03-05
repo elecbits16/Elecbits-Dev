1e216671def8989f2913e2aeb926ef3295f5201c
aurav7 <saurav.rav67@gmail.com>
2017-03-05 16:34:06 +0530

    quiz added

--- learn/quiz.sql
+++ learn/quiz.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 19, 2017 at 12:49 AM
+-- Generation Time: Mar 05, 2017 at 09:33 AM
 -- Server version: 5.7.11
 -- PHP Version: 5.6.19
 
@@ -28,12 +28,34 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `quiz` (
   `quiz_id` varchar(255) DEFAULT NULL,
-  `question` text,
-  `opt_1` text,
-  `opt_2` text,
-  `opt_3` text,
-  `opt_4` text,
-  `correct_opt` text,
+  `question_id1` varchar(255) DEFAULT NULL,
+  `question1` text,
+  `opt_A1` text,
+  `opt_B1` text,
+  `opt_C1` text,
+  `opt_D1` text,
+  `correct_opt1` text,
+  `question_id4` varchar(455) DEFAULT NULL,
+  `question4` text,
+  `opt_A4` text,
+  `opt_B4` text,
+  `opt_C4` text,
+  `opt_D4` text,
+  `correct_opt4` text,
+  `question_id3` varchar(355) DEFAULT NULL,
+  `question3` text,
+  `opt_A3` text,
+  `opt_B3` text,
+  `opt_C3` text,
+  `opt_D3` text,
+  `correct_opt3` text,
+  `question_id2` varchar(255) DEFAULT NULL,
+  `question2` text,
+  `opt_A2` text,
+  `opt_B2` text,
+  `opt_C2` text,
+  `opt_D2` text,
+  `correct_opt2` text,
   `quiz_lesson_id` varchar(255) DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
@@ -41,8 +63,8 @@ CREATE TABLE `quiz` (
 -- Dumping data for table `quiz`
 --
 
-INSERT INTO `quiz` (`quiz_id`, `question`, `opt_1`, `opt_2`, `opt_3`, `opt_4`, `correct_opt`, `quiz_lesson_id`) VALUES
-('QZ23', '<p>Are you alright ?</p>', 'yess', 'nose', 'mayb', 'Mere mehboobc', 'Mere mehboos', 'LS235');
+INSERT INTO `quiz` (`quiz_id`, `question_id1`, `question1`, `opt_A1`, `opt_B1`, `opt_C1`, `opt_D1`, `correct_opt1`, `question_id4`, `question4`, `opt_A4`, `opt_B4`, `opt_C4`, `opt_D4`, `correct_opt4`, `question_id3`, `question3`, `opt_A3`, `opt_B3`, `opt_C3`, `opt_D3`, `correct_opt3`, `question_id2`, `question2`, `opt_A2`, `opt_B2`, `opt_C2`, `opt_D2`, `correct_opt2`, `quiz_lesson_id`) VALUES
+('QZ24', '1', '<p>What is your name ?</p>', 'john', 'sue', 'lucy', 'adam', 'A', '4', '<p>what is cricket</p>', 'MSD', 'VIRAT', 'ABD', 'Kane', 'A', '3', '<p>What is bank</p>', 'Tyra', 'Naomi', 'Namia', 'Johhny', 'D', '2', '<p>What is your fav singer ?</p>', 'pitbull', 'katy', 'enrique', 'AKON', 'C', 'LS4');
 
 --
 -- Indexes for dumped tables
